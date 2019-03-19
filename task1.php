2e50b03f641bdd7c2ed4567b7c0604a8babce355
vvv vvvvv <vvv@vvv.com>
2019-03-19 23:22:08 +0900

    コメント追加

--- task1.php
+++ task1.php
@@ -1,5 +1,6 @@
 <h1>1人掲示板</h1>
 
+<!-- GETリクエストでの表示内容 ※URLに「?name=〇〇」か「?message=〇〇」を付加して投げる -->
 <h4>
 <?php
   if(isset($_GET['name']) || isset($_GET['message'])){
@@ -8,6 +9,7 @@
 ?>
 </h4>
 
+<!-- POSTリクエストでの表示内容 ※URLには何も付加しない -->
 <h4><?php
   if (isset($_POST['name'])) {
     $name = $_POST['name'];
