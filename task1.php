2555158f98680f9e3f10f626bd68424de555db6e
vvv vvvvv <vvv@vvv.com>
2019-03-20 22:30:15 +0900

    課題再修正コミット

--- task1.php
+++ task1.php
@@ -1,37 +1,23 @@
 <h1>1人掲示板</h1>
 
-<!-- GETリクエストでの表示内容 ※URLに「?name=〇〇」か「?message=〇〇」を付加して投げる -->
-<h4>
 <?php
-  if(isset($_GET['name']) || isset($_GET['message'])){
-    echo '一回だけ投稿できます';
-  }
-?>
-</h4>
-
-<!-- POSTリクエストでの表示内容 ※URLには何も付加しない -->
-<h4><?php
-  if (isset($_POST['name'])) {
-    $name = $_POST['name'];
-    echo $name.'さんの投稿';
-  }
-?></h4>
-<?php
-  if(isset($_POST['message'])){
-    $message = $_POST['message'];
-    echo $message;
+  if($_SERVER['REQUEST_METHOD'] == "POST"){
+    echo '<h3>'.$_POST['name'].'さんの投稿</h3>';
+    echo $_POST['message'];
+  }else{
+    echo '<h3>一回だけ投稿できます</h3>';
   }
 ?>
 
 <form action="task1.php" method="post">
   <br>
-  Name
+  <label for="name">Name</label>
   <br>
-  <input type="text" name="name" required>
+  <input type="text" name="name" id="name" required>
   <br>
-  Message
+  <label for="message">Message</label>
   <br>
-  <input type="text" name="message" required>
+  <input type="text" name="message" id="message" required>
   <br><br>
   <input type="submit" value="送信">
 </form>
